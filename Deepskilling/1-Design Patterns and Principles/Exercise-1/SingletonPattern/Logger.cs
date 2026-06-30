d08becd695ea5ea57700abaf8711111bcb10966c
KM Sai Sowmya <kmsaisowmya@gmail.com>
2026-06-30 19:01:55 +0530

    Implemented Singleton Pattern using Logger

---
+++ Deepskilling/1-Design Patterns and Principles/Exercise-1/SingletonPattern/Logger.cs
@@ -0,0 +1,35 @@
+namespace SingletonPattern
+{
+    public sealed class Logger
+    {
+
+        private static Logger? instance;
+
+
+        private Logger()
+        {
+
+        }
+
+
+        public static Logger Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Logger();
+                }
+
+                return instance;
+            }
+        }
+
+
+        public void Log(string message)
+        {
+            Console.WriteLine("Log: " + message);
+        }
+
+    }
+}
---
+++ Deepskilling/1-Design Patterns and Principles/Exercise-1/SingletonPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonPattern;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.Instance;
+        logger1.Log("First log message");
+        Logger logger2 = Logger.Instance;
+        logger2.Log("Second log message");
+        if(logger1 == logger2)
+        {
+            Console.WriteLine("Only one Logger instance exists.");
+        }
+    }
+}
